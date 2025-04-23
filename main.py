d01440bdfb3bf0db3af7ff99b8c1f68387c2a4b1
Harish Toppae <harish.toppae@gmail.com>
2025-04-23 15:53:45 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+import json
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
+app = FastAPI()
+
+
+def generator():
+    i = 1
+    while i <= 10:
+        yield json.dumps({"session": i, "content": f"Session {i}"}) + "\n"
+        i += 1
+        time.sleep(1)
+
+
+def sse_generator():
+    i = 1
+    while i <= 10:
+        data = {"message": f"Session {i}"}
+        yield f"data: {json.dumps(data)}\n\n"
+        i += 1
+        time.sleep(1)
+
+
+@app.get("/")
+async def main():
+    return "Welcome to SSE Test"
+
+
+@app.get("/notsse")
+async def notsse():
+    return StreamingResponse(generator(), media_type="application/json")
+
+
+@app.get("/sse")
+async def sse():
+    return StreamingResponse(sse_generator(), media_type="text/event-stream")
