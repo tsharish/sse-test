b59241e22492c0c33db89e023a54e49f7cc04890
Harish Toppae <harish.toppae@gmail.com>
2025-04-23 17:00:14 -0400

    add X-Content-Type-Options header

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
-headers = {"Cache-Control": "no-store"}
+headers = {"Cache-Control": "no-store", "X-Content-Type-Options": "nosniff"}
 
 
 def generator():
