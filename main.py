6f59d3c19dcd1cd74562f0ece21fd8e1828c586c
Harish Toppae <harish.toppae@gmail.com>
2025-04-24 21:31:59 -0400

    add starlette-sse

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
+from sse_starlette import EventSourceResponse
 
 app = FastAPI()
 
@@ -40,6 +41,15 @@ def sse_generator():
         time.sleep(1)
 
 
+def event_generator():
+    i = 1
+    while i <= 10:
+        data = {"message": f"Session {i}"}
+        yield f"{json.dumps(data)}"
+        i += 1
+        time.sleep(1)
+
+
 @app.get("/")
 async def main():
     return "Welcome to SSE Test"
@@ -63,3 +73,13 @@ async def plain():
 @app.get("/web")
 async def read_index():
     return FileResponse("index.html")
+
+
+@app.get("/event")
+async def event():
+    return EventSourceResponse(event_generator())
+
+
+@app.get("/webevent")
+async def read_index():
+    return FileResponse("index1.html")
